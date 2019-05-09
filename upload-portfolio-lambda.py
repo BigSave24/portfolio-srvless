00da3b81e800f89036c1ae51862e35f1b90dcfdd
Kevin Edmond <Kedmond24@live.com>
2019-05-09 18:29:18 -0400

    ADD portfolio upload script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,30 @@
+import boto3
+import StringIO
+import zipfile
+from botocore.client import Config
+import mimetypes
+
+
+#         #### Setup connection to s3 account
+s3 = boto3.resource('s3')
+#         #### Used with botocore.client
+# s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+
+#        #### Setup name and connection to s3 buckets
+portfolio_bucket = s3.Bucket('portfolio.kevinedmond.info')
+build_bucket = s3.Bucket('portfoliobuild.kevinedmond.info')
+
+
+ #       #### Download updated portfolio files
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+
+#       #### Extract, open, upload files, and set ACL permissions
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm,
+            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
