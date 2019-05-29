0850bb0b0adc90bab2ca3a5475ffd85b218d36cc
Kevin Edmond <Kedmond24@live.com>
2019-05-28 21:02:22 -0400

    UPDATE portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,27 +4,26 @@ import zipfile
 from botocore.client import Config
 import mimetypes
 
-
-#         #### Setup connection to s3 account
-s3 = boto3.resource('s3')
-#         #### Used with botocore.client
-# s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
-
+def lambda_handler(event, context):
+#         #### Setup connection to s3 account #Used with botocore.client#
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
 #        #### Setup name and connection to s3 buckets
-portfolio_bucket = s3.Bucket('portfolio.kevinedmond.info')
-build_bucket = s3.Bucket('portfoliobuild.kevinedmond.info')
+    portfolio_bucket = s3.Bucket('portfolio.kevinedmond.info')
+    build_bucket = s3.Bucket('portfoliobuild.kevinedmond.info')
 
-
- #       #### Download updated portfolio files
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
+#       #### Download updated portfolio files
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
 #       #### Extract, open, upload files, and set ACL permissions
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    print "Job Done!"
+
+    return 'Hello from Lambda!'
