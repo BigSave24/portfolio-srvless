f3ef30fb6447a121859bb4e331745599213a2fc3
Kevin Edmond <Kedmond24@live.com>
2019-06-03 00:55:44 -0400

    UPDATE moved static markup for work examples into React components

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Tribute Website",
+    'image': {
+      'desc': "screenshot of my Bob Marley tribute website",
+      'src': "images/project1.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Sports Catalog Web Application",
+    'image': {
+      'desc': "screenshot of my sports catalog web app",
+      'src': "images/project2.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Color Coded Web Application",
+    'image': {
+      'desc': "screenshot of my color coded web app",
+      'src': "images/project3.png",
+      'comment': ""
+    }
+  }
+];
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
